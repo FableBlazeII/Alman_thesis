f9640f5398c6e5b6ae92780dda0747fbbaec0fa7
FableBlaze <oolop7@hotmail.com>
2013-03-09 14:00:23 +0200

    Testandmed (50 proovi ja 10000 sõna) Testandmete genereerimiseks kasutatud programm

---
+++ Testandmed/generateTestData.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import random, os;
+
+wordLen=12
+wordCount=10000
+sampleCount=100
+OUTPUT="./inputSummary.txt"
+countMax=500
+
+
+letters=['A','C','D','E','F','G','H','I','K','L','M','N','P','Q','R','S','T','V','W','Y']
+possibleCounts=range(countMax)+[0]*(countMax*2)
+
+print "Output: ",
+print os.path.abspath(OUTPUT)
+with open(OUTPUT, 'w') as output:
+    for i in xrange(wordCount-1):
+        tmpLetters=[]
+        for j in range(wordLen):
+            tmpLetters.append(letters[random.randint(0,19)])
+        tmpWord=''.join(tmpLetters)
+
+        tmpCounts=[]
+        for k in range(sampleCount):
+            tmpCounts.append(str(possibleCounts[random.randint(0,len(possibleCounts)-1)]))
+        output.write(tmpWord)
+        output.write('\t')
+        output.write('\t'.join(tmpCounts))
+        output.write('\n')
+            
+    tmpLetters=[]
+    for j in range(wordLen):
+        tmpLetters.append(letters[random.randint(0,19)])
+    tmpWord=''.join(tmpLetters)
+
+    tmpCounts=[]
+    for k in range(sampleCount):
+        tmpCounts.append(str(possibleCounts[random.randint(0,len(possibleCounts)-1)]))
+    output.write(tmpWord)
+    output.write('\t')
+    output.write('\t'.join(tmpCounts))
+
+print "Done"
